a0d7679acd917d8e6ba5f368846bd1472aa24e9b
Yuriy Husnay <husnaj23@gmail.com>
2017-01-13 13:27:27 +0200

    cache assets list

--- src/api.js
+++ src/api.js
@@ -1,6 +1,13 @@
 const AERIAL_URL = 'http://a1.phobos.apple.com/us/r1000/000/Features/atv/AutumnResources/videos/entries.json';
 
-export function getAssets () {
+const MAX_CACHE_AGE = 60 * 60 * 1000; // 1 hour
+
+export function fetchAssets () {
+  const cache = localStorage.getItem('assetsCache');
+  const cacheTimestamp = JSON.parse(localStorage.getItem('assetsCacheTimestamp'));
+
+  if (cache && cacheTimestamp > Date.now() - MAX_CACHE_AGE) return Promise.resolve(JSON.parse(cache));
+
   return fetch(AERIAL_URL, {
     method: 'GET',
     mode: 'cors',
@@ -8,5 +15,12 @@ export function getAssets () {
       'Access-Control-Allow-Origin': '*',
       'Content-Type': 'text/plain'
     }
-  }).then(response => response.text()).then(JSON.parse);
+  })
+    .then(response => response.text())
+    .then(response => {
+      localStorage.setItem('assetsCache', response);
+      localStorage.setItem('assetsCacheTimestamp', Date.now());
+      return response;
+    })
+    .then(JSON.parse);
 }
